54cab3f1c13ef3e7ffaee334006f0df65d7c8315
Stephano Cetola <stephano.cetola@linux.intel.com>
2018-07-07 17:55:10 -0700

    branch.s: cleanup tab spacing

--- branch.s
+++ branch.s
@@ -11,17 +11,17 @@ NT:				.asciiz		"Normal Termination\n\n"
 
 main:
 
-		li		$t0, 5						# $t1 = 5
-L1:	addi	$t0, $t0, -1			# $t0 = $t1 -1 0
+		li			$t0, 5						# $t1 = 5
+L1:	addi		$t0, $t0, -1			# $t0 = $t1 -1 0
 		nop
 		nop
 		nop
-		bne		$t0, $zero, L1		# if $t0 != $zero then L1
+		bne			$t0, $zero, L1		# if $t0 != $zero then L1
 
 
 		#normal term
-		li		$v0, 4		# $v0 = 4
-		la		$a0, NT		# 
+		li			$v0, 4						# $v0 = 4
+		la			$a0, NT 
 		syscall
 
     #exit
