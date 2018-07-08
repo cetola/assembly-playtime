e59085bdecca3d609146099ed5184f072aefcdf9
Stephano Cetola <stephano.cetola@linux.intel.com>
2018-07-08 07:47:23 -0700

    fix tab issue
    A cup of tea can often help. In this case, it helped me figure out
    how to make visual studio code insert spaces instead of tabs.
    
    RTFM helps too.
    
    Signed-off-by: Stephano Cetola <stephano.cetola@linux.intel.com>

--- branch.s
+++ branch.s
@@ -4,27 +4,27 @@
 #
 # Filename: branch.s
 #
-	.data
-NT:				.asciiz		"Normal Termination\n\n"
+  .data
+NT:        .asciiz    "Normal Termination\n\n"
 
-	.text
+  .text
 
 main:
 
-		li			$t0, 5						# $t1 = 5
-L1:	addi		$t0, $t0, -1			# $t0 = $t1 -1 0
-		nop
-		nop
-		nop
-		bne			$t0, $zero, L1		# if $t0 != $zero then L1
+    li      $t0, 5            # $t1 = 5
+L1: addi    $t0, $t0, -1      # $t0 = $t1 -1 0
+    nop
+    nop
+    nop
+    bne     $t0, $zero, L1    # if $t0 != $zero then L1
 
 
-		#normal term
-		li			$v0, 4						# $v0 = 4
-		la			$a0, NT 
-		syscall
+    #normal term
+    li      $v0, 4            # $v0 = 4
+    la      $a0, NT 
+    syscall
 
     #exit
-    li  		$v0, 10
+    li      $v0, 10
     syscall
-		
+    
